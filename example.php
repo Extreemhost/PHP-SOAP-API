155b5c2dd017d17a664f7971f8ee4e8b9d5f6467
Extreemhost <admin@extreemhost.nl>
2015-09-13 14:48:19 +0200

    Update example.php

--- example.php
+++ example.php
@@ -4,6 +4,15 @@ include_once('class/class.soapclient.php');
 # make new soap object
 $SoapClient = new PHPSoapClient();
 
+# any arguments?
+$CallParams = array(
+    'username'	=> '',
+    'password'	=> ''
+);
+
+# make the soap call
+echo $SoapClient->_callSoapClient('login', $CallParams);
+
 $CallClient = array('securecode' => '');
 
 # make the soap call
